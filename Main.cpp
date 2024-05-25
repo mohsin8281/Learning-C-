3e34f2edb7c92fd0828b9548d977cf0c5210a5b2
Muhammad Moazam Ali <mmoazamali74@gmail.com>
2024-05-25 12:33:39 +0500

    I have Done Day 2 Work

--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,17 @@
 
 #include<iostream> //Library Function
 using namespace std; //Statndered Libary Fucntion
-int main() // Main Funciton
+void main() // Main Funciton
 {
 	cout << "Hello world"; //Out
+
+
+	//Day2
+
+	int a = 2323; // This is integer Data Type
+
+	cout << a; //I am print a
+	
 }
+
+
